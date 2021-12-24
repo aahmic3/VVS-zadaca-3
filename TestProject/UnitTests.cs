1a41f3d8073e721f852fb7180c20e7c3a5c52267
Dzeneta Ahmic <dahmic3@etf.unsa.ba>
2021-12-24 23:31:00 +0100

    Dodana probna tesna metoda

--- TestProject/UnitTests.cs
+++ TestProject/UnitTests.cs
@@ -1,13 +1,29 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Cvjecara;
+using System;
 namespace TestProject
 {
     [TestClass]
     public class UnitTests
     {
         [TestMethod]
-        public void TestMethod1()
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestTuning()
         {
+            Cvjeæara cvjeæara = new Cvjeæara();
+            for(int i = 0; i < 10000; i++)
+            {
+                cvjeæara.RadSaCvijeæem(
+                    new Cvijet(Vrsta.Ruža, "Rosa"+ i, "Crvena",System.DateTime.Now.AddDays(-2),i+1),
+                    0, i+1);
+            }
+            //breakpoint - prije poziva metode
+            int x = 0;
+            cvjeæara.RadSaCvijeæem(
+                new Cvijet(Vrsta.Ruža, "Rosa5000", "Crvena", System.DateTime.Now.AddDays(-2), 100),
+                0, 1);
+            //breakpoint - poslije poziva metode
+            int y = 0;
         }
     }
 }
