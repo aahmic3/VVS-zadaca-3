61394ab610d5ea685725288a34f7377ebd44047d
Dzeneta Ahmic <dahmic3@etf.unsa.ba>
2021-12-24 22:33:24 +0100

    Dodan testni projekat za 2. zadatak

---
+++ TestProject/UnitTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cvjecara;
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
